77e0b1d9f44c45c056a76e40d2821cb0c74fa79a
baasirishfaq <baasirishfaq@gmail.com>
2024-01-15 00:37:09 +0530

    leetcode 2798

--- homework.cpp
+++ homework.cpp
@@ -1,18 +1,15 @@
 class Solution
 {
 public:
-    int numIdenticalPairs(vector<int> &nums)
+    int numberOfEmployeesWhoMetTarget(vector<int> &hours, int target)
     {
-        int n = nums.size();
+        int n = hours.size();
         int count = 0;
         for (int i = 0; i < n; i++)
         {
-            for (int j = i + 1; j < n; j++)
+            if (hours[i] >= target)
             {
-                if (nums[i] == nums[j])
-                {
-                    count++;
-                }
+                count++;
             }
         }
         return count;
