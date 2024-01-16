31aa408f40d894211757196ea42ea9f100f701fd
baasirishfaq <baasirishfaq@gmail.com>
2024-01-16 23:35:01 +0530

    leetcode 2974. Minimum Number Game

--- homework.cpp
+++ homework.cpp
@@ -1,37 +1,13 @@
-#include <iostream>
-#include <stdio.h>
-
-using namespace std;
-
-bool isSorted(int arr[], int size)
+class Solution
 {
-    if (size == 1)
+public:
+    vector<int> numberGame(vector<int> &nums)
     {
-        return true;
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size(); i += 2)
+        {
+            swap(nums[i], nums[i + 1]);
+        }
+        return nums;
     }
-    if (arr[0] > arr[1])
-    {
-        return false;
-    }
-    int smallerArray[size - 1];
-    for (int i = 1; i < size; i++)
-    {
-        smallerArray[i - 1] = arr[i];
-    }
-    bool isSmallerSorted = isSorted(smallerArray, size - 1);
-    if (isSmallerSorted)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
-
-int main()
-{
-
-    int arr[] = {1, 6, 3, 4};
-    cout << isSorted(arr, 4) << endl;
-}
+};
