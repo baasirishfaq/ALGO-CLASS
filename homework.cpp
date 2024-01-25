6942c9d1fa3e2d307f6cbc5048e34c8296976f10
baasirishfaq <baasirishfaq@gmail.com>
2024-01-25 23:18:09 +0530

    ..

--- homework.cpp
+++ homework.cpp
@@ -1,12 +1,7 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
 int main()
 {
-    string city = "delhi";
-    string name("james");
-
-    cout << city << endl;
-    cout << name;
-    return 0;
-} //
+}
