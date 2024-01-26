a9b03d9cdd23d3bb2708b5deffaefab179b5378b
baasirishfaq <baasirishfaq@gmail.com>
2024-01-26 21:45:03 +0530

    class

--- homework.cpp
+++ homework.cpp
@@ -2,6 +2,28 @@
 
 using namespace std;
 
+// classes...
+
+class Student
+{
+public:
+    int roll_no;
+    char gender;
+    int mobile;
+    int standard;
+};
+
 int main()
 {
+
+    Student john;
+    cin >> john.roll_no;
+    cin >> john.gender;
+    cin >> john.mobile;
+    cin >> john.standard;
+
+    cout << john.roll_no;
+    cout << john.gender;
+    cout << john.mobile;
+    cout << john.standard;
 }
