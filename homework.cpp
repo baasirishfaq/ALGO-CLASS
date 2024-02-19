fdc99ba926eff3b9c0968c19b6265b65c3176f2f
baasirishfaq <baasirishfaq@gmail.com>
2024-02-19 23:16:36 +0530

    prac

--- homework.cpp
+++ homework.cpp
@@ -1,26 +1,9 @@
-class Solution
-{
-public:
-    int maximumDifference(vector<int> &nums)
-    {
-        int ans = 0, currans = 0, n = nums.size();
+#include <bits/stdc++.h>
 
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = i + 1; j < n; j++)
-            {
-                if (nums[j] > nums[i])
-                {
-                    currans = nums[j] - nums[i];
-                    if (currans > ans)
-                    {
-                        ans = currans;
-                    }
-                }
-            }
-        }
-        if (ans <= 0)
-            return -1;
-        return ans;
-    }
-};
+using namespace std;
+
+int main()
+{
+    cout << " hello ";
+    main();
+}
