7872eb880fc901e85944c6e43d6904e99613061c
baasirishfaq <baasirishfaq@gmail.com>
2024-02-20 23:56:51 +0530

    leetcode max subarray avg

--- homework.cpp
+++ homework.cpp
@@ -1,9 +1,28 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
-int main()
+class Solution
 {
-    cout << " hello ";
-    main();
-}
+public:
+    double findMaxAverage(vector<int> &nums, int k)
+    {
+        int sum = 0;
+        for (int i = 0; i < k; i++)
+        {
+            sum += nums[i];
+        }
+        int maxsum = sum;
+
+        int start_index = 0;
+        int end_index = k;
+
+        while (end_index < nums.size())
+        {
+            sum = sum - nums[start_index];
+            start_index++;
+
+            sum = sum + nums[end_index];
+            end_index++;
+
+            maxsum = max(maxsum, sum);
+        }
+        return (double)maxsum / k;
+    }
+};
