80a71b5fec4f13375e53e5f4865ab4427dce9f99
baasirishfaq <baasirishfaq@gmail.com>
2024-03-07 00:00:10 +0530

    .

--- homework.cpp
+++ homework.cpp
@@ -1,32 +1,8 @@
-class Solution {
-public:
-    int maxVowels(string s, int k) 
-    {
-        unordered_set <char> vowels = {'a', 'e', 'i', 'o', 'u'};
+#include <bits/stdc++.h>
 
-        int maxvowels = 0, windowvowels = 0;
+using namespace std;
 
-        for(int i = 0; i<k; i++)
-        {
-            if(vowels.contains(s[i]))
-            {
-                windowvowels ++;
-            }
-        }
-        maxvowels = windowvowels;
+int main()
+{
 
-        for(int i = k; i<s.size(); i++)
-        {
-            if(vowels.contains(s[i-k]))
-            {
-                windowvowels--;
-            }
-            if(vowels.contains(s[i]))
-            {
-                windowvowels++;
-            }
-            maxvowels = max(maxvowels, windowvowels);
-        }
-        return maxvowels;
-    }
-};
+} //
