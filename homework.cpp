ed3db758c0fc4f9745dab48e6b3ba3322f040f3b
baasirishfaq <baasirishfaq@gmail.com>
2024-03-12 23:28:07 +0530

    linkedlist implementation

--- homework.cpp
+++ homework.cpp
@@ -2,7 +2,32 @@
 
 using namespace std;
 
+struct node
+{
+    int data;
+    node *next;
+};
+
+void printlist(node *n)
+{
+    while (n != NULL)
+    {
+        cout << n->data << endl;
+        n = n->next;
+    }
+}
+
 int main()
 {
-    //
+    node *head = new node;
+    node *second = new node;
+    node *third = new node;
+    head->data = 1;
+    head->next = second;
+    second->data = 2;
+    second->next = third;
+    third->data = 3;
+    third->next = NULL;
+
+    printlist(head);
 }
