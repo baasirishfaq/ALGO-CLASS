37d80946b3a4595638827977b9de5470fe141988
baasirishfaq <baasirishfaq@gmail.com>
2024-03-15 00:21:15 +0530

    stack declaration

--- liveclassdojo.cpp
+++ liveclassdojo.cpp
@@ -2,42 +2,20 @@
 
 using namespace std;
 
-struct node
-{
-    int data;
-    node *next;
-};
-
-node *deletenode(node *head)
-{
-    if (head == NULL)
-        return head;
-    node *temp = head;
-    head = head->next;
-    delete temp;
-    return head;
-}
-
-void printlist(node *n)
-{
-    while (n != NULL)
-    {
-        cout << n->data << endl;
-        n = n->next;
-    }
-}
-
 int main()
 {
-    node *head = new node;
-    node *second = new node;
-    node *third = new node;
-    head->data = 1;
-    head->next = second;
-    second->data = 2;
-    second->next = third;
-    third->data = 3;
-    third->next = NULL;
-    head = deletenode(head);
-    printlist(head);
+    vector<int> st;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        st.push_back(x);
+    }
+    cout << "following items in stack:";
+    for (int i = 0; i < n; i++)
+    {
+        cout << st[i] << " ";
+    }
 }
