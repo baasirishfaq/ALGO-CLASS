1783c67bb88522d5021e1d7da74e80f9191810f6
baasirishfaq <baasirishfaq@gmail.com>
2024-03-18 03:06:14 +0530

    lc prac

--- liveclassdojo.cpp
+++ liveclassdojo.cpp
@@ -1,49 +1,50 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
 int main()
 {
-    string s("leet**cod*e");
-    vector<char> st;
-
-    st.push_back(s[0]);
-
-    for (int i = 0; i < s.size(); i++)
     {
-        if (s[i + 1] == '*')
-        {
-            st.pop_back();
-        }
-        else
-        {
-            st.push_back(s[i + 1]);
-        }
-    }
-    string ans(st.begin(), st.end());
-    cout << ans << endl;
-}
+        string s("zabx#bhec###ab####z");
+        string t("ad#c##zaz");
 
-//
+        string st = "";
+        string st1 = "";
 
-class Solution
-{
-public:
-    string removeStars(string s)
-    {
-        vector<char> st;
-        for (int i = 0; i < s.size(); i++)
+        // Loop until both strings are exhausted
+        for (int i = 0, j = 0; i < s.size(), j < t.size(); i++, j++)
         {
-            if (s[i] == '*')
+            // Process string s
+            if (i < s.size() && s[i] == '#')
             {
-                st.pop_back();
+                if (!st.empty())
+                {
+                    st.pop_back();
+                }
             }
-            else
+            else if (i < s.size())
             {
                 st.push_back(s[i]);
             }
+
+            // Process string t
+            if (j < t.size() && t[j] == '#')
+            {
+                if (!st1.empty())
+                {
+                    st1.pop_back();
+                }
+            }
+            else if (j < t.size())
+            {
+                st1.push_back(t[j]);
+            }
         }
-        string stx(st.begin(), st.end());
-        return stx;
+        cout << st << endl;
+        cout << st1 << endl;
+
+        // Check if the modified strings are equal
+        bool x = st == st1;
+        cout << x << endl;
     }
-};
+    return 0;
+}
