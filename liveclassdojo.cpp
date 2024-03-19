471e9a76915c1052608dd11753670dac0a8e23ad
baasirishfaq <baasirishfaq@gmail.com>
2024-03-19 23:55:27 +0530

    .

--- liveclassdojo.cpp
+++ liveclassdojo.cpp
@@ -1,21 +1,37 @@
-class Solution
+#include <bits/stdc++.h>
+
+using namespace std;
+//
+
+int main()
 {
-public:
-    string removeStars(string s)
+    int n;
+    cin >> n;
+    int nums[n];
+    cout << "enter vals in numsay" << endl;
+    for (int i = 0; i < n; i++)
     {
-        vector<char> st;
-        for (int i = 0; i < s.size(); i++)
+        cin >> nums[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (nums[i] < nums[i + 1])
         {
-            if (s[i] == '*')
+            for (int j = 0; j < n; j++)
             {
-                st.pop_back();
-            }
-            else
-            {
-                st.push_back(s[i]);
+                if (nums[j] > nums[i])
+                {
+                    nums[i] = nums[j];
+                }
             }
         }
-        string stx(st.begin(), st.end());
-        return stx;
+        else
+        {
+            nums[i] = -1;
+        }
     }
-};
+    for (int i = 0; i < n; i++)
+    {
+        cout << nums[i] << " ";
+    }
+}
