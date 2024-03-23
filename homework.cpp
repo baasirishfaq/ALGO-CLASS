19ddf650026b6cd89bd81cbb67c525cf41d0ff1f
baasirishfaq <baasirishfaq@gmail.com>
2024-03-24 00:07:54 +0530

    unordered set

--- homework.cpp
+++ homework.cpp
@@ -2,30 +2,17 @@
 
 using namespace std;
 
-// THIS IS BUBBLE SORT
-
 int main()
 {
-    int arr[16] = {4, 5, 1, 5, 6, 7, 4, 3, 2, 6, 8, 2, 98, 43, 12, 55};
+    // Initialize
+    unordered_set<int> s;
 
-    for (int i = 0; i < 16; i++)
-    {
-        for (int j = 0; j < 16; j++)
-        {
-            if (arr[i] > arr[j])
-            {
-                swap(arr[i], arr[j]);
-                // If the element at index 'i' is greater than the element at index 'j', then swap them
+    s.insert(10);
+    s.insert(20);
+    s.insert(30);
 
-                // int temp = arr[i];
-                // arr[i] = arr[j];
-                // arr[j] = temp;
-            }
-        }
-    }
-    // print the result
-    for (int i = 0; i < 16; i++)
+    for (auto it = s.begin(); it != s.end(); it++)
     {
-        cout << arr[i] << " ";
+        cout << *it << " ";
     }
 }
