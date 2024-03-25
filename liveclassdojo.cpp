42f76671f53aad70cfd7978f0dee066f20ae8e8b
baasirishfaq <baasirishfaq@gmail.com>
2024-03-25 23:27:14 +0530

    hashset to find occurence of elements

--- liveclassdojo.cpp
+++ liveclassdojo.cpp
@@ -4,11 +4,19 @@ using namespace std;
 
 int main()
 {
-    int arr[5] = {1, 2, 3, 4, 5};
+    int arr[] = {0, 1, 2, 3, 4, 5, 6, 9, 9, 9, 9, 2, 2, 4};
+
     int n = sizeof(arr) / sizeof(arr[0]);
 
-    for (int i = 3; i < n; i++)
+    unordered_map<int, int> umap;
+    for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
     {
-        cout << arr[i] << " " << endl;
+        int key = arr[i];
+        umap[key]++;
     }
-} //
+    for (auto it = umap.begin(); it != umap.end(); it++)
+    {
+        cout << it->first << " is " << it->second << " times" << endl;
+    }
+    cout << "N is " << n;
+}
