c3051dbe5ff18a935f8942854b4b234ed5570867
baasirishfaq <baasirishfaq@gmail.com>
2024-03-29 22:59:42 +0530

    map to find element frequency

--- liveclassdojo.cpp
+++ liveclassdojo.cpp
@@ -1,22 +1,20 @@
-class Solution
-{
-public:
-    int subarraySum(vector<int> &nums, int k)
-    {
-        unordered_map<int, int> map1;
-        int sum = 0;
-        map1[0] = 1;
-        int subarrays = 0;
+#include <bits/stdc++.h>
 
-        for (int i = 0; i < nums.size(); i++)
-        {
-            sum += nums[i];
-            if (map1.count(sum - k))
-            {
-                subarrays += map1[sum - k];
-            }
-            map1[sum] += 1;
-        }
-        return subarrays;
+using namespace std;
+
+int main()
+{
+    map<int, int> mp;
+
+    int arr[10] = {1, 1, 1, 1, 2, 2, 3, 4, 4, 10};
+
+    for (int i = 0; i < 10; i++)
+    {
+        int key = arr[i];
+        mp[key]++;
     }
-};
+    for (auto it = mp.begin(); it != mp.end(); it++)
+    {
+        cout << it->first << " " << it->second << endl;
+    }
+}
