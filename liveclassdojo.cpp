82b055e1bd42a70be333d0b6ed7b1be4ec48ca97
baasirishfaq <baasirishfaq@gmail.com>
2024-03-31 00:39:15 +0530

    hashing prac

--- liveclassdojo.cpp
+++ liveclassdojo.cpp
@@ -1,16 +1,20 @@
-class Solution
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
 {
-public:
-    int singleNumber(vector<int> &nums)
+    vector<int> nums = {12, 34, 56, 78, 90, 23, 45, 67};
+
+    unordered_map<int, int> hashValues;
+
+    for (auto num : nums)
     {
-        unordered_set<int> numset;
-        for (int num : nums)
-        {
-            if (numset.count(num))
-                numset.erase(num);
-            else
-                numset.insert(num);
-        }
-        return *numset.begin();
+        hashValues[num] = num % 10;
     }
-};
+
+    for (auto pair : hashValues)
+    {
+        cout << pair.first << " " << pair.second << endl;
+    }
+}
