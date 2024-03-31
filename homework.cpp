b7ac0bad5c6cf258891da7897396531d31cffcdc
baasirishfaq <baasirishfaq@gmail.com>
2024-04-01 00:43:33 +0530

    majority element leetcode

--- homework.cpp
+++ homework.cpp
@@ -1,31 +1,21 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
-int main()
+class Solution
 {
-    // Initialize
-    unordered_set<int> s;
-
-    s.insert(10);
-    s.insert(20);
-    s.insert(30);
-
-    // to iterate over the elements in the map
-    for (auto it = s.begin(); it != s.end(); it++)
+public:
+    int majorityElement(vector<int> &nums)
     {
-        cout << *it << endl;
+        unordered_map<int, int> mp;
+        int ans = 0, n = nums.size() / 2;
+        for (int num : nums)
+        {
+            mp[num]++;
+        }
+        for (auto a : mp)
+        {
+            if (a.second > n)
+            {
+                ans = a.first;
+            }
+        }
+        return ans;
     }
-    // to find the size of the map;
-    cout << "the size of the map is " << s.size() << endl;
-
-    int key = 10;
-    if (s.find(key) != s.end())
-    {
-        cout << "the key is present in the set" << endl;
-    }
-    else
-    {
-        cout << "the key is not present in the set" << endl;
-    }
-}
+};
