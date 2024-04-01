9206e0de0230618292a1ec945245a5c34fa6f4bd
baasirishfaq <baasirishfaq@gmail.com>
2024-04-02 00:33:58 +0530

    roman to integer leetcode

--- homework.cpp
+++ homework.cpp
@@ -1,21 +1,37 @@
-class Solution
+#include <iostream>
+#include <unordered_map>
+#include <string>
+using namespace std;
+
+int main()
 {
-public:
-    int majorityElement(vector<int> &nums)
+    unordered_map<char, int> m;
+    m['I'] = 1;
+    m['V'] = 5;
+    m['X'] = 10;
+    m['L'] = 50;
+    m['C'] = 100;
+    m['D'] = 500;
+    m['M'] = 1000;
+
+    string s;
+    cout << "Enter a Roman numeral: ";
+    cin >> s;
+
+    int ans = 0;
+    for (int i = 0; i < s.size(); i++)
     {
-        unordered_map<int, int> mp;
-        int ans = 0, n = nums.size() / 2;
-        for (int num : nums)
+        if (m[s[i]] < m[s[i + 1]])
         {
-            mp[num]++;
+            ans -= m[s[i]];
         }
-        for (auto a : mp)
+        else
         {
-            if (a.second > n)
-            {
-                ans = a.first;
-            }
+            ans += m[s[i]];
         }
-        return ans;
     }
-};
+
+    cout << "Integer representation: " << ans << endl;
+
+    return 0;
+}
