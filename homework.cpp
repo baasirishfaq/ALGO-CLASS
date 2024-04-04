ce7e60eaaab449ed9d93c92cb956ad3f660bfa9f
baasirishfaq <baasirishfaq@gmail.com>
2024-04-04 23:51:09 +0530

    leetcode first unique char in string

--- homework.cpp
+++ homework.cpp
@@ -1,18 +1,37 @@
-class Solution
-{
-public:
-    int climbStairs(int n)
-    {
-        if (n == 0 || n == 1)
-            return 1;
+#include <iostream>
+#include <unordered_map>
+#include <string>
 
-        int arr[n + 1];
-        arr[1] = 1;
-        arr[2] = 2;
-        for (int i = 3; i <= n; i++)
-        {
-            arr[i] = arr[i - 1] + arr[i - 2];
-        }
-        return arr[n];
+using namespace std;
+
+int firstUniqChar(string s)
+{
+    unordered_map<char, int> mp;
+    for (char c : s)
+    {
+        mp[c]++;
     }
-};
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (mp[s[i]] == 1)
+        {
+            return i;
+        }
+    }
+    return -1; // If no unique character found
+}
+
+int main()
+{
+    string s = "leetcode";
+    int index = firstUniqChar(s);
+    if (index != -1)
+    {
+        cout << "Index of the first unique character: " << index << endl;
+    }
+    else
+    {
+        cout << "No unique character found in the string." << endl;
+    }
+    return 0;
+}
