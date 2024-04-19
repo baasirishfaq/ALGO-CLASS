ed047c5766c45eb0f5f86e5dc1434d1defa8a439
baasirishfaq <baasirishfaq@gmail.com>
2024-04-19 23:45:36 +0530

    count frequency of words

--- liveclassdojo.cpp
+++ liveclassdojo.cpp
@@ -1,19 +1,38 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <unordered_map>
+#include <string>
 
 using namespace std;
-class Solution
+
+int main()
 {
-public:
-    int lengthOfLastWord(string s)
+    unordered_map<string, int> mp;
+    string text("Hello world, this is a simple sentence. Hello again, world!");
+    string word;
+
+    for (char c : text)
     {
-        int count = 0;
-        for (int i = s.size() - 1; i >= 0; i--)
+
+        if (isalpha(c))
         {
-            if (s.at(i) != ' ')
-                count++;
-            else if (count > 0)
-                break;
+            word += c;
         }
-        return count;
+        else if (!word.empty())
+        {
+            mp[word]++;
+            word.clear();
+        }
     }
-};
+
+    if (!word.empty())
+    {
+        mp[word]++;
+    }
+
+    for (auto pair : mp)
+    {
+        cout << pair.first << ": " << pair.second << endl;
+    }
+
+    return 0;
+}
