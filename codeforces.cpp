074bc56d14d6a1768674de8222bb7d7fbfe86197
Baasir Ishfaq <baasirishfaq@gmail.com>
2024-12-23 22:33:37 -0500

    just some basic dsa

--- codeforces.cpp
+++ codeforces.cpp
@@ -1,74 +1,24 @@
-#include <iostream>
+#include <bits/stdc++.h>
+
 using namespace std;
 
 int main()
 {
-    int rows = 5, cols = 5; // Define dimensions of the matrix
-    int matrix[5][5];
-    int matrixCenter = (rows * cols / 2) + 1;
-    int targetindex;
-    int noofmoves;
-    int colsecure = 0;
-    int rowz, colz;
 
-    // Input the matrix
-    cout << "Enter a 5x5 matrix:" << endl;
-    for (int i = 0; i < rows; i++)
+    int arr[3] = {2, 0, 1};
+
+    for (int i = 0; i < 3; i++)
     {
-
-        for (int j = 0; j < cols; j++)
+        for (int j = i + 1; j < 3; j++)
         {
-            cout << "enter element at row " << i + 1 << " and col " << j + 1 << " =";
-            cin >> matrix[i][j];
-        }
-    }
-
-    // Print the matrix to verify
-    cout << "The matrix is:" << endl;
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < cols; j++)
-        {
-            cout << matrix[i][j] << " "; // Print each element
-        }
-        cout << endl; // Move to the next row
-    }
-    for (int i = 0; i < rows; i++)
-    {
-
-        for (int j = 0; j < cols; j++)
-        {
-            if (matrix[i][j] == 1)
+            if (arr[i] > arr[j])
             {
-                // flag = 1;
-                cout << "target element found at row " << i + 1 << " and column " << j + 1 << endl;
-                targetindex = (i + 1) * (j + 1);
-                colsecure = j + 1;
-                rowz = i + 1;
-                colz = j + 1;
+                swap(arr[i], arr[j]);
             }
         }
     }
-    cout << "target index  is " << targetindex << endl;
-    if (targetindex % 3 == 0)
+    for (int i = 0; i < 3; i++)
     {
-        cout << "if condition entered" << endl;
-        noofmoves = abs(colsecure - 3);
+        cout << arr[i] << " ";
     }
-    else if (targetindex > 15)
-    {
-        noofmoves = abs(matrixCenter - targetindex) / 3;
-    }
-    else if (targetindex < 6)
-    {
-        noofmoves = abs(rowz * colz) - 9;
-    }
-    else
-    {
-        noofmoves = abs(matrixCenter - targetindex);
-    }
-    cout << noofmoves << endl;
-    cout << "matrixcenter found = " << matrixCenter;
-    cout << "target index multiply is found = " << targetindex;
-    return 0;
-}
+};
