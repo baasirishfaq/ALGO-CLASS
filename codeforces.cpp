4d7dedb9c44e9ec10e47b7254f6f9a6a708c103f
Baasir Ishfaq <baasirishfaq@gmail.com>
2024-12-27 03:10:14 -0500

    791A bear and big brother codeforces

--- codeforces.cpp
+++ codeforces.cpp
@@ -1,27 +1,21 @@
 #include <bits/stdc++.h>
-#include <windows.h>
 
 using namespace std;
 
 int main()
 {
-    while (true)
+    int limak, bob, ctr = 0;
+    cin >> limak;
+    cin >> bob;
+
+    while (limak <= bob)
     {
-        system("cls");
-
-        char timeinformat[50];
-        time_t rawtime;
-        time(&rawtime);
-
-        struct tm *currentime;
-
-        currentime = localtime(&rawtime);
-
-        // cout << currentime->tm_hour << endl;
-
-        strftime(timeinformat, 50, "%I:%M:%S %p", currentime);
-        cout << timeinformat << endl;
-
-        Sleep(1000);
+        if (limak <= bob)
+        {
+            bob = bob * 2;
+            limak = limak * 3;
+            ctr++;
+        }
     }
+    cout << ctr;
 };
