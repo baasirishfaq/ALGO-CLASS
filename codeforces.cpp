6ebfb8e74878d2ec9054cc23a9acf1fe41fe1272
Baasir Ishfaq <baasirishfaq@gmail.com>
2025-02-11 14:06:42 -0500

    1pe

--- codeforces.cpp
+++ codeforces.cpp
@@ -1,21 +1,34 @@
-class Solution {
-public:
-    int maxProfit(vector<int>& prices) 
+#include <stdio.h>
+#include <stdlib.h>
+
+void sumofodd(int *arr, int n)
+{
+    int sum = 0;
+    for (int i = 0; i < n; i++)
     {
-        int maxprofit = 0;
-        for(int i = 0; i < prices.size(); i++)
+        if (arr[i] % 2 != 0)
         {
-            if(prices[i])
+            sum += arr[i];
         }
-        for(int i = 0; i < prices.size(); i++)
-        {
-            for(int j = i+1; j<prices.size(); j++)
-            {
-                int profit = prices[j] - prices[i];
-                maxprofit = max(profit, maxprofit);
-            }
-        }
-        if (maxprofit < 0) return 0;
-        return maxprofit;    
     }
-};
+    printf("the sum of odd numbers in the array is %d", sum);
+}
+
+int main()
+{
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int *arr = (int *)malloc(n * sizeof(int));
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter elements in the array");
+        scanf("%d", &arr[i]);
+    }
+
+    sumofodd(arr, n);
+
+    free(arr);
+}
