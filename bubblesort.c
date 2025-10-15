8fb6600610d2285b77a4b72dfccbc2899c705d64
Baasir Ishfaq <baasirishfaq@gmail.com>
2025-10-15 17:12:15 -0400

    binary search

--- bubblesort.c
+++ bubblesort.c
@@ -1,3 +1,49 @@
+#include <stdio.h>
+ 
+
+// INSERTION SORT
+// int main()
+// {
+//     int a[8] = {3,6,1,7,4,5,6,9};
+
+//     for(int i = 1; i<8-1; i++)
+//     {
+//         int j = i;
+//         while(j>0 && (a[j-1] > a[j]))
+//         {
+//                 int temp = a[j];
+//                 a[j] = a[j-1];
+//                 a[j-1] = temp;
+//                 j = j-1;
+//         }
+//     }
+//     for(int i = 0; i<8; i++)
+//     {
+//         printf("%d ", a[i]);
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // #include <stdio.h>
  
 
